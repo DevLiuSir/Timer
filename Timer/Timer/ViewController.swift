b914709e94a287986fe7e11b8ff8a8d83b57ccfe
liuchuan <liuchuan@MacBook-Pro.local>
2016-06-11 17:53:12 +0800

    add annotation

--- Timer/Timer/ViewController.swift
+++ Timer/Timer/ViewController.swift
@@ -10,27 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var timerLabel: UILabel!
+    
     var timerCount = 0
     var timerRunning = false
     var timer = NSTimer()
-    
-    
-    @IBOutlet weak var timerLabel: UILabel!
+
     
     func Counting() {
         
         timerCount += 1
-        
         timerLabel.text = "\(timerCount)"
         
     }
-    
-    
+  
+    // ====================
+    // MARK: SATRT_BUTTON
+    // ====================
     @IBAction func startButton(sender: UIButton) {
         
         if timerRunning == false {
             
-            //调用NSTimer的类方法scheduledTimerWithTimeInterval:target:selector:userInfo:repeats: 创建并启动一个每1秒钟调用1次Counting:方法的timer，并将返回的实例保存到timer中。
+            // 调用NSTimer的类方法 scheduledTimerWithTimeInterval 创建并启动一个每1秒钟调用1次Counting:方法的timer.
+            // 并将返回的实例保存到timer中。
             timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.Counting), userInfo: nil, repeats: true)
             
             timerRunning = true
@@ -39,23 +41,28 @@ class ViewController: UIViewController {
         
     }
 
+    // ====================
+    // MARK: STOP_BUTTON
+    // ====================
     @IBAction func stopButton(sender: UIButton) {
         
         if timerRunning == true {
             
             // 无效
             timer.invalidate()
-            
             timerRunning = false
             
         }
         
     }
     
+    // ====================
+    // MARK: RESTART_BUTTON
+    // ====================
     @IBAction func restart(sender: UIButton) {
+        
         // 复位
         timerCount = 0
-        
         timerLabel.text = "0"
         
     }
@@ -63,7 +70,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
